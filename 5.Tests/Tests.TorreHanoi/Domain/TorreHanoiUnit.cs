442a1245348e390445af90f1676259715eb0da0d
Paulo Coelho Filho <paulo_coe@hotmail.com>
2017-10-22 22:30:07 -0200

    Correção de testes unitarios do dominio TorreHanoi

--- 5.Tests/Tests.TorreHanoi/Domain/TorreHanoiUnit.cs
+++ 5.Tests/Tests.TorreHanoi/Domain/TorreHanoiUnit.cs
@@ -23,14 +23,34 @@ namespace Tests.TorreHanoi.Domain
         [TestCategory(CategoriaTeste)]
         public void Construtor_Deve_Retornar_Sucesso()
         {
-            Assert.Fail();
+            var torre = new global::Domain.TorreHanoi.TorreHanoi(3, _mockLogger.Object);
+
+            Assert.IsNotNull(torre);
+            Assert.IsNotNull(torre.Id);
+            Assert.AreEqual(torre.Discos.Count, 3);
+            Assert.IsNotNull(torre.Destino);
+            Assert.IsNotNull(torre.Intermediario);
+            Assert.IsNotNull(torre.Origem);
+            Assert.IsNotNull(torre.DataCriacao);
+            Assert.AreEqual(torre.Status, global::Domain.TorreHanoi.TipoStatus.Pendente);
+            Assert.IsNotNull(torre.PassoAPasso);
+            Assert.AreEqual(torre.Origem.Tipo, global::Domain.TorreHanoi.TipoPino.Origem);
+            Assert.AreEqual(torre.Destino.Tipo, global::Domain.TorreHanoi.TipoPino.Destino);
+            Assert.AreEqual(torre.Intermediario.Tipo, global::Domain.TorreHanoi.TipoPino.Intermediario);
+            Assert.AreEqual(torre.Intermediario.Discos.Count, 0);
+            Assert.AreEqual(torre.Destino.Discos.Count, 0);
+            Assert.AreEqual(torre.Origem.Discos.Count, 3);
         }
 
         [TestMethod]
         [TestCategory(CategoriaTeste)]
         public void Processar_Deverar_Retornar_Sucesso()
         {
-            Assert.Fail();
+            var torre = new global::Domain.TorreHanoi.TorreHanoi(3, _mockLogger.Object);
+            torre.Processar();
+
+            Assert.AreEqual(torre.Status, global::Domain.TorreHanoi.TipoStatus.FinalizadoSucesso);
+            Assert.IsNotNull(torre.DataFinalizacao);
         }
     }
 }
